158cbff47e3e828c2d42fb18579cdb06782e4013
Stiven Ramírez Arango <sramirez@anutibara.com>
2019-12-26 12:14:38 -0500

    Add and Delete Items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 
 import { AngularFireModule } from '@angular/fire';
@@ -9,18 +11,25 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 
+import { ConnectionService } from './services/connection.service';
+import { ListComponent } from './components/list/list.component';
+import { ListAddComponent } from './components/list-add/list-add.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    ListAddComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule,
     AngularFireStorageModule
   ],
-  providers: [],
+  providers: [ConnectionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/list/list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ConnectionService } from 'src/app/services/connection.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+
+  items: any[] = [];
+
+  constructor(private connection: ConnectionService ) { 
+    this.connection.listItems().subscribe(item => {
+      this.items = item;
+      console.log(this.items);
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  delete(item) {
+    this.connection.deleteItem(item);
+  }
+
+}
